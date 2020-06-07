8104bd3eddbef7fc765588f8e6c46f1960ff0946
Hannes Riechert <hannes@tychons.net>
2020-06-07 22:00:24 +0200

    Generalize u1_drive2 to allow setting amplitude and phase of drive

--- c/u1_drive2.c
+++ c/u1_drive2.c
@@ -1,9 +1,13 @@
 /**
- * Drive outputs at given frequencies, optionally
- * trigger GPIO initializer and record waveform at ADC.
+ * Drive outputs at given frequencies and and phase and record ADC.
+ * Output and recording triggered by  negative edge of external trigger DIO0_P
+ * of extension connector E1 (3.3V, negative edge).
  *
- * Usage:
- *     u1_drive2 FREQ1 FREQ2 [init]
+ * Provides trigger on digital pin DIO0_N of
+ * extension connector E1 (3.3V, negative edge).
+ * Connect DIO0_N to DIO0_P to synchronize.
+ *
+ * Usage: u1_drive2 FREQ1 AMP1 PHASE1 FREQ2 AMP2 PHASE2
  */
 
 #include <stdio.h>
@@ -18,18 +22,18 @@
 #include "utility.h"
 
 
-// Additional settling time for output in microseconds
-#define OUTPUT_SETTLING_TIME 10e3
-
-
 int main(int argc, char **argv) {
     // Parse arguments
-    if (argc < 3 || argc > 4) {
+    if (argc != 7) {
+        fprintf(stderr, "Invalid arguments.\n");
         exit(1);
     }
-    float fout1 = atof(argv[1]);
-    float fout2 = atof(argv[2]);
-    bool gpio_trig = argc == 4;
+    float freq1 = atof(argv[1]);
+    float amp1 = atof(argv[2]);
+    float phase1 = atof(argv[3]);
+    float freq2 = atof(argv[4]);
+    float amp2 = atof(argv[5]);
+    float phase2 = atof(argv[6]);
 
     // Initialize IO.
     if (rp_Init() != RP_OK) {
@@ -37,47 +41,50 @@ int main(int argc, char **argv) {
         exit(2);
     }
 
-    if (gpio_trig) { // Prepare trigger
-      rp_DpinSetDirection(RP_DIO0_P, RP_IN);
-      rp_DpinSetDirection(RP_DIO0_N, RP_OUT);
-      rp_DpinSetState(RP_DIO0_N, RP_HIGH);
-    }
+    // Prepare GPIO trigger
+    rp_DpinSetDirection(RP_DIO0_P, RP_IN);
+    rp_DpinSetDirection(RP_DIO0_N, RP_OUT);
+    rp_DpinSetState(RP_DIO0_N, RP_HIGH);
 
     // Initialize outputs
     rp_GenReset();
-    rp_GenFreq(RP_CH_1, fout1);
-    rp_GenFreq(RP_CH_2, fout2);
-    rp_GenAmp(RP_CH_1, 0.8);
-    rp_GenAmp(RP_CH_2, 0.8);
-    rp_GenOffset(RP_CH_1, 0);
-    rp_GenOffset(RP_CH_2, 0);
+    rp_GenTriggerSource(RP_CH_1, RP_GEN_TRIG_SRC_EXT_NE);
+    rp_GenTriggerSource(RP_CH_2, RP_GEN_TRIG_SRC_EXT_NE);
     rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
     rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);
-    rp_GenMode(RP_CH_1, RP_GEN_MODE_CONTINUOUS);
-    rp_GenMode(RP_CH_2, RP_GEN_MODE_CONTINUOUS);
+    rp_GenFreq(RP_CH_1, freq1);
+    rp_GenFreq(RP_CH_2, freq2);
+    rp_GenAmp(RP_CH_1, amp1);
+    rp_GenAmp(RP_CH_2, amp2);
+    rp_GenPhase(RP_CH_1, phase1);
+    rp_GenPhase(RP_CH_2, phase2);
+    rp_GenOffset(RP_CH_1, 0);
+    rp_GenOffset(RP_CH_2, 0);
+    rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
+    rp_GenMode(RP_CH_2, RP_GEN_MODE_BURST);
+    rp_GenBurstCount(RP_CH_1, -1); // -1: continuous
+    rp_GenBurstCount(RP_CH_2, -1); // -1: continuous
 
     // Setup both ADC channels
     rp_AcqReset();
-    rp_AcqSetGain(RP_CH_1, RP_LOW);
-    rp_AcqSetGain(RP_CH_2, RP_LOW);
+    rp_AcqSetGain(RP_CH_1, RP_HIGH);
+    rp_AcqSetGain(RP_CH_2, RP_HIGH);
     rp_AcqSetDecimation(RP_DEC_8);
     rp_AcqSetTriggerSrc(RP_TRIG_SRC_EXT_NE);
-    rp_AcqSetTriggerDelay(7992); // max 8192
+    rp_AcqSetTriggerDelay(7992); // trigger at sample 200
     rp_AcqSetAveraging(1);
     rp_AcqStart();
 
     rp_GenOutEnable(RP_CH_1);
     rp_GenOutEnable(RP_CH_2);
-    usleep(OUTPUT_SETTLING_TIME);
 
-    // wait for "look ahead" buffer to fill up
-    uint32_t buffertime = RP_BUFFER_SIZE * 8 / 125; // us
+    // Wait for "look ahead" buffer to fill up
+    uint32_t buffertime = ADC_BUFFER_SIZE * 8 / 125; // us
     usleep(buffertime);
 
-    // trigger
-    if (gpio_trig) rp_DpinSetState(RP_DIO0_N, RP_LOW);
-    // wait until acquisition trigger fired
-    // (they do not have to be connected).
+    // Fire trigger
+    rp_DpinSetState(RP_DIO0_N, RP_LOW);
+    // Wait until acquisition trigger fired
     rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
     while (1) {
         rp_AcqGetTriggerState(&state);
@@ -87,18 +94,19 @@ int main(int argc, char **argv) {
     }
     usleep(buffertime);
 
+    rp_GenOutDisable(RP_CH_1);
+    rp_GenOutDisable(RP_CH_2);
+
     // Retrieve data
-    uint32_t bufsize1 = RP_BUFFER_SIZE;
-    float *buf1 = (float *)malloc(RP_BUFFER_SIZE * sizeof(float));
+    uint32_t bufsize1 = ADC_BUFFER_SIZE;
+    float *buf1 = (float *)malloc(ADC_BUFFER_SIZE * sizeof(float));
     rp_AcqGetOldestDataV(RP_CH_1, &bufsize1, buf1);
 
-    uint32_t bufsize2 = RP_BUFFER_SIZE;
-    float *buf2 = (float *)malloc(RP_BUFFER_SIZE * sizeof(float));
+    uint32_t bufsize2 = ADC_BUFFER_SIZE;
+    float *buf2 = (float *)malloc(ADC_BUFFER_SIZE * sizeof(float));
     rp_AcqGetOldestDataV(RP_CH_2, &bufsize2, buf2);
 
     rp_AcqReset();
-    rp_GenOutDisable(RP_CH_1);
-    rp_GenOutDisable(RP_CH_2);
 
     // Print data to stdout
     for(uint32_t i = 0; i < bufsize1 && i < bufsize2; i++){
