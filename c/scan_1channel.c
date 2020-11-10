63795ad2833eec7bab17bf380fbdd4316018f8df
Hannes Riechert <hannes@tychons.net>
2020-11-10 13:20:26 +0100

    scan_1channel: use more flexible cmd line arguments; calculate signal 2-1

--- c/scan_1channel.c
+++ c/scan_1channel.c
@@ -5,22 +5,25 @@
  * Decimation factor for sampling rate is chosen such that the
  * waveform is sampled by at least 20 samples per period.
  *
- * Usage: ./run.sh IP scan_1channel.x F_START F_END STEPS [full]
+ * Usage: ./run.sh IP scan_1channel.x F_START,STEPS,F_END [full]
  *
  * Where start and end frequencies F_START and F_END are floats in
  * units of Hertz, and STEPS is an integer (steps between start and
- * end frequency on log scale).  Use optional flag `full` to output
+ * end frequency on log scale).  Instead of range also a single number
+ * can be supplied.  Use optional flag `full` to output
  * complete ADC buffers instead of demodulated data.
  *
  * Prints demodulated data to stdout in 13 columns
  * (whitespaces / column separators are tabs):
  *
- *     f samplerate A1 A2 A22 ph2 ph22 dc1 dc2 dc22 err1 err2 err22
+ *     f samplerate A1 A2 A12 A22 ph2 ph12 ph22 dc1 dc2 dc12 dc22 err1 err2 err12 err22
  *
- * where ph2 is (phase2-phase1) and ph22 is (phase22-phase1*2). dc1,
- * dc2, and dc22 are the DC offsets and err the reconstruction errors
- * (sqrt of mean of squares of deviation) respectively.  Output data
- * comes with header.
+ * Numbers indicate: 1 for CH 1, 2 for CH 2, 12 for CH2-CH1,
+ * 22 for CH2 at double frequency.
+ * The global phase is not well defined and ph2 is (phase2-phase1)
+ * ph22 is (phase22-phase1*2). dc1, dc2, and dc22 are the DC offsets
+ * and err the reconstruction errors (sqrt of mean of squares of deviation) respectively.
+ * Output data comes with header.
  *
  * With flag `full` outputs
  *
@@ -47,13 +50,16 @@
 
 int main(int argc, char **argv) {
     // Parse arguments
-    if (argc < 4 || argc > 5) {
+    if (argc < 2 || argc > 3) {
         exit(1);
     }
-    float fstart = atof(argv[1]);
-    float fend = atof(argv[2]);
-    int nsteps = atoi(argv[3]);
-    bool fulldata = argc == 5;
+    float fstart, fend;
+    int nsteps;
+    if (!parse_cmd_line_range(argv[1], &fstart, &fend, &nsteps)) {
+        fprintf(stderr, "Invalid argument.\n");
+        exit(1);
+    }
+    bool fulldata = argc == 3;
 
     // Initialize IO.
     if (rp_Init() != RP_OK) {
@@ -63,7 +69,8 @@ int main(int argc, char **argv) {
     // Initialize outputs
     rp_GenReset();
     rp_GenFreq(RP_CH_1, fstart);
-    rp_GenAmp(RP_CH_1, 0.5);
+    rp_GenAmp(RP_CH_1, 1);
+    rp_GenOffset(RP_CH_1, 0);
     rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
     rp_GenMode(RP_CH_1, RP_GEN_MODE_CONTINUOUS);
     // Trigger setup only needed in burst mode.
@@ -72,9 +79,10 @@ int main(int argc, char **argv) {
     // Allocate data buffers
     float *buf1 = (float*)malloc(RP_BUFFER_SIZE * sizeof(float));
     float *buf2 = (float*)malloc(RP_BUFFER_SIZE * sizeof(float));
+    float *buf12 = (float*)malloc(RP_BUFFER_SIZE * sizeof(float));
 
     if (! fulldata)
-        printf("f\tsamplerate\tA1\tA2\tA22\tph2\tph22\tdc1\tdc2\tdc22\terr1\terr2\terr22\n");
+        printf("f\tsamplerate\tA1\tA2\tA12\tA22\tph2\tph12\tph22\tdc1\tdc2\tdc12\tdc22\terr1\terr2\terr12\terr22\n");
 
     // Scan
     float samplerate;
@@ -93,26 +101,36 @@ int main(int argc, char **argv) {
         acquire_2channels(dec, buf1, &s1, buf2, &s2);
         rp_GenOutDisable(RP_CH_1);
 
+        for (uint32_t j = 0; j < RP_BUFFER_SIZE; j++) {
+            buf12[j] = buf2[j] - buf1[j];
+        }
+
         if (! fulldata) {
-            float A1, A2, A22;
-            float phase1, phase2, phase22, ph2, ph22;
-            float offset1, offset2, offset22;
-            float sd1, sd2, sd22;
+            float A1, A2, A12, A22;
+            float phase1, phase2, phase12, phase22, ph2, ph12, ph22;
+            float offset1, offset2, offset12, offset22;
+            float sd1, sd2, sd12, sd22;
             demodulate(buf1, s1, f, samplerate, &A1, &phase1, &offset1);
             demodulate(buf2, s2, f, samplerate, &A2, &phase2, &offset2);
             demodulate(buf2, s2, 2*f, samplerate, &A22, &phase22, &offset22);
+            demodulate(buf12, (s1 < s2)? s1 : s2, f, samplerate, &A12, &phase12, &offset12);
             sd1  = deviation_from_reconstruction(buf1, s1, samplerate, f, A1, phase1, offset1);
             sd2  = deviation_from_reconstruction(buf2, s2, samplerate, f, A2, phase2, offset2);
             sd22 = deviation_from_reconstruction(buf2, s2, samplerate, 2*f, A22, phase22, offset22);
+            sd12  = deviation_from_reconstruction(buf12, (s1 < s2)? s1 : s2, samplerate, f, A12, phase12, offset12);
             // phase difference of CH2 - CH1 in range [-pi, pi]
             ph2 = fmod(phase2 - phase1 + M_PI, 2*M_PI) - M_PI;
+            ph12 = fmod(phase12 - phase1 + M_PI, 2*M_PI) - M_PI;
             // phase difference of CH1 and CH2 @ double frequency in range [-pi to pi]
             ph22 = fmod(phase22 - fmod(phase1*2, 2*M_PI) + M_PI, 2*M_PI) - M_PI;
-            printf("%e\t%f\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\n", f, samplerate, A1, A2, A22,
-                   ph2, ph22, offset1, offset2, offset22, sd1, sd2, sd22);
+            printf("%e\t%f\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\n",
+                   f, samplerate, A1, A2, A12, A22,
+                   ph2, ph12, ph22,
+                   offset1, offset2, offset12, offset22,
+                   sd1, sd2, sd12, sd22);
 
-            fprintf(stderr, "%5.1f mV  %5.1f mV  %5.1f mV\n",
-                    1e3*A1, 1e3*A2, 1e3*A22);
+            fprintf(stderr, "%5.1f mV  %5.1f mV  %5.1f mV  %5.1f mV\n",
+                    1e3*A1, 1e3*A2, 1e3*A12, 1e3*A22);
         } else {
             printf("%f\t%f\t1", f, samplerate);
             for (uint32_t k = 0; k < s1; k++)
